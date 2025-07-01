3448843ac5f2c4ba971879764a71c2f099c35ab5
Francisco Garcia <franciscojgarciapinero@gmail.com>
2025-07-01 01:53:42 +0000

    Tarea API spotify

--- src/app.py
+++ src/app.py
@@ -6,5 +6,41 @@ from dotenv import load_dotenv
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+
+# 5K4W6rqBFWDnAN6FQUkS6x kanye west
+
+#Obtener las mejores canciones del artista
+
+results = spotify.artist_top_tracks("5K4W6rqBFWDnAN6FQUkS6x")
+
+
+
+songs = []
+
+for track in results['tracks']:
+    songs.append({"name":track['name'],
+                  "popularity":track['popularity'],
+                  "duracion_min":track['duration_ms']/60000})
+
+
+
+df= pd.DataFrame(songs)
+
+print(df)
+
+
+import matplotlib 
+
+plt.scatter(df['duracion_min'],df['popularity'],)
+
+plt.show
